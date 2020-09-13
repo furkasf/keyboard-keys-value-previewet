ad06bc7a1814f9fa3f99e55a0005d327612cca88
Furkan Tural <furkantural3328@hotmail.com>
2020-09-13 04:43:56 +0300

    event and funtion write each propery

---
+++ script.js
@@ -0,0 +1,11 @@
+const chars = document.getElementById('keys');
+const printChars = (n) => document.querySelector('#keys').value = n;//print function
+
+chars.addEventListener('keydown', (e) =>{
+    let keyCode = e.keyCode;
+    let keyValue = e.location;
+    let metachar = e.metaKey
+    document.querySelector('#key-value').innerHTML = `key code: ${keyCode} location: ${keyValue} metachar: ${metachar}`
+    printChars('')//prevent to much char on input box and focusing one char
+});
+
